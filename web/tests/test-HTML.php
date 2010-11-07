e0a708b9378e1b874e16e3b34f966b6608266b47
Nicholas Istre <nick@teamworkadgroup.com>
2010-11-07 03:02:07 -0600

    Created most of a HTML page generator class. Added jQuery.

---
+++ web/tests/test-HTML.php
@@ -0,0 +1,9 @@
+<?php
+require_once '../libs/class/HTML.inc';
+
+$html = new HTML("Testing Page");
+
+$html->add_script_links("../js/jquery.js", "../js/cartographer.min.0.4.js");
+
+$html->add("<h1>Header 1</h1>");
+$html->add("<p>Testing the HTML class.</p>");
