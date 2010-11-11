60fd421c11f9ed4cafe7eba65cad50ec43645292
Nicholas Istre <nick@teamworkadgroup.com>
2010-11-10 20:49:24 -0600

    Changed AMF PHP to return random data for all states.

--- web/amfphp/services/test/Test.php
+++ web/amfphp/services/test/Test.php
@@ -1,24 +1,26 @@
 <?php
 class Test {
 	
+	private $state_list = "AL,AK,AS,AZ,AR,CA,CO,CT,DE,DC,FM,FL,GA,GU,HI,ID,IL,IN,IA,KS,KY,LA,ME,MH,MD,MA,MI,MN,MS,MO,MT,NE,NV,NH,NJ,NM,NY,NC,ND,MP,OH,OK,OR,PW,PA,PR,RI,SC,SD,TN,TX,UT,VT,VI,VA,WA,WV,WI,WY";
+	
+	private function generate_test_data() {
+		$state_array = explode(",", $this->state_list);
+		
+		$test_data_array = array();
+		
+		foreach ($state_array as $state_code) {
+			$state_data[] = array(
+				"region" => "US-{$state_code}",
+				"val" => rand(0, 100),
+				"label" => $state_code,
+			);
+		}
+		
+		return $state_data;
+	}
+	
 	public function return_test_data($val1, $val2 = null, $val3 = null) {
-		$test_data = array (
-			array (
-				"region" => "US-CA",
-				"val" => "10",
-				"label" => $val1,
-			),
-			array (
-				"region" => "US-OR",
-				"val" => "15",
-				"label" => $val2,
-			),
-			array(
-				"region" => "US-WA",
-				"val" => "20",
-				"label" => $val3,
-			),
-		);
+		$test_data = $this->generate_test_data();
 		
 		return $test_data;
 	}
